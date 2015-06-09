e35b775f7aba1e7e1f52a85dd03455b9c7821e61
Information School <ischoolit@uw.edu>
2015-06-08 20:25:15 -0700

    created a function

--- aggregate.js
+++ aggregate.js
@@ -5,62 +5,18 @@ window.Agg = (function () {
 		    
 	var agg = {
 		avg: function(data, col1, col2, col3) {
-			var avgData = [];
-			var sum = 0;
-			var count = 0;
-			var keys = Object.keys(data[0]);
-			//console.log(keys);
-			var agg1 = 0;
-			var aggMap = new Map();
-			var objectArray = [];
-			data.forEach(function (d) {
-				var value = d[col1] + " " + d[col2];
-				//console.log(value);
-				var rData = [];
-				if(aggMap.has(value)){
-					
-					//console.log(aggMap.get(value));
-					//console.log(d);
-					//console.log("here");
-					var column = keys[i];
-					var update = aggMap.get(value);
-					update["count"]++;
-					var count = update["count"];
-
-					sum = aggMap.get(value).agg + +d[col3];
-					//console.log(aggMap.get(value).agg + " " + d[col2]);
-					agg1 = sum / count;
-					//console.log(sum + " " + count);
-					update["agg"] = agg1;
-
-					aggMap.set(value, update);
-				
-
-				} else {
-					for(var i = 0; i <keys.length; i++) {
-						var column = keys[i];					
-						rData[keys[i]] = d[keys[i]];
-					}
-					rData["agg"] = +d[col3];
-					rData["count"] = 1;
-					aggMap.set(d[col1] + " " + d[col2], rData);
-				}
-	
- 					//console.log(aggMap.get(d[col1]));
-			});
+			
 			var counter = 0;
-			console.log(aggMap);
+			var aggMap = aggregate(data, col1, col2, col3);
+			console.log(aggregate(data, col1, col2, col3));
+			var objectArray = [];
+			
+			//console.log(aggMap);
 			for (var k in aggMap) {
 				console.log("hello");
-				/*var obj = toObject(aggMap[k]);
-				objectArray[counter] = obj;
-				console.log("test");
-			  //objectArray[counter] = aggMap.get(k);
-			  
-			  counter++; */
-			}
-
-
+				var obj = toObject(aggMap[k]);
+				objectArray.push(obj);			 
+			} 
 
 			return objectArray;
 		}
@@ -76,67 +32,51 @@ function toObject(arr) {
   return rv;
 }
 
-//Have user define the file and the aggregate choice 
-function getColumns(fileName, column1, column2, choice) {
-	var columns = new Map();
-	d3.csv(fileName, function(error, tennis) {
-		if (error) {
-			return console.log(error);
-		}
-		//var columns = new Map();
-		var headerNames = d3.keys(tennis[0]);
-		for(var i = 0; i < headerNames.length; i++) {
-			columns.set(headerNames[i], []);
-		}
-		//console.log(columns);		
-		//console.log(headerNames);
-		tennis.forEach(function(d) { //Convert data to Numbers
+function aggregate(data, col1, col2, col3) {
+	var avgData = [];
+	var sum = 0;
+	var count = 0;
+	var keys = Object.keys(data[0]);
+	//console.log(keys);
+	var agg1 = 0;
+	var aggMap = new Map();
+	var objectArray = [];
+	data.forEach(function (d) {
+		var value = d[col1] + " " + d[col2];
+		//console.log(value);
+		var rData = [];
+		if(aggMap.has(value)){
+			
+			//console.log(aggMap.get(value));
 			//console.log(d);
+			//console.log("here");
+			var column = keys[i];
+			var update = aggMap.get(value);
+			update["count"]++;
+			var count = update["count"];
 
-			var count = 0;
-			var sum = 0;
-			var average = 1;
-			var min = 0;
-			var max = 0;
-			var agg = 0;
-			for(i = 0; i <headerNames.length; i++) {
-				//columns.get(headerNames[i]).push(d[headerNames[i]]);
-				var value = d[headerNames[i]];
+			sum = aggMap.get(value).agg + +d[col3];
+			//console.log(aggMap.get(value).agg + " " + d[col2]);
+			agg1 = sum / count;
+			//console.log(sum + " " + count);
+			update["agg"] = agg1;
 
-				if(aggMap.has(column1)){
-					count++;
-					sum += aggMap.get(column1);
-					if (choice == "sum" && value == "column2"){
-						agg = sum;
-					} else if (choice == "count" && value == "column2"){
-						agg = count;
-					} else if (choice == "average" && value == "column2"){
-						agg = sum / count;
-					} else if (choice == "min" && value == "column2") {
+			aggMap.set(value, update);
+		
 
-					} else if (choice == "max" && value == "column2"){
-
-					}
-
-					aggMap.set(d[column1], {
-						choice: agg,
-						headerNames: value,
-					})
-
-				} else {
-					aggMap.set(d[column1], {
-						choice: agg,
-						headerNames: value,
-					})
-				}
-
-				
+		} else {
+			for(var i = 0; i <keys.length; i++) {
+				var column = keys[i];					
+				rData[keys[i]] = d[keys[i]];
 			}
-			//addToMap(d, column1, column2, choice);
-		});
-	});
+			rData["agg"] = +d[col3];
+			rData["count"] = 1;
+			aggMap.set(d[col1] + " " + d[col2], rData);
+		}
 
-	return columns;
+				//console.log(aggMap.get(d[col1]));
+	});
+	return aggMap;
 }
 
 function sum (data){
