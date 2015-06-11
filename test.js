f1e0cf0335608293a6fe10ab72000a6da4a06845
Information School <ischoolit@uw.edu>
2015-06-11 16:02:37 -0700

    added code for visualization

--- test.js
+++ test.js
@@ -16,6 +16,74 @@
 			// console.log(Agg.review(tennis, "Winner", "WPts", "Matosevic M."));
 			console.log(Agg.avg(tennis, "WPts", ["Winner", "Series", "Surface"]));
 			console.log(Agg.max(tennis, "WPts", ["Winner"]));
+			console.log(Agg.count(tennis, ["Winner"]));
+			var winCount = Agg.count(tennis, ["Winner"]);
+			console.log(Agg.max(winCount, "Count", ["Winner"]));
+
+var margin = {top: 20, right: 20, bottom: 30, left: 70};
+    var w = 960 - margin.left - margin.right;
+    var h = 500 - margin.top - margin.bottom;
+	
+var format = d3.time.format("%b %Y");
+var dataset;
+/*var x = d3.scale.ordinal()
+	.domain(["Europe", "Asia", "America", "Africa", "Oceania", "Not Specified "])
+    .rangeRoundBands([0, w - margin.left - margin.right], .1);*/
+/*var x = d3.scale.linear()
+	.domain([1820, 2000])
+	.range([0, w]);*/
+	
+var y = d3.scale.linear()
+        .domain([0, 68])
+        .range([h, 0]);
+ 
+ var x = d3.scale.ordinal()
+    .rangeRoundBands([0, w - margin.left - margin.right], .1);
+ x.domain(winCount.map(function(d) { return d.Winner; }));
+
+
+
+
+
+var tooltip = d3.select("body").append("div")     
+	.attr("class", "tooltip")     
+	.style("opacity", 0);  
+
+var col = d3.scale.category10();
+
+
+		
+var xAxis = d3.svg.axis()
+	.scale(x)
+	.orient("bottom");
+	
+
+		
+var yAxis = d3.svg.axis()
+	.scale(y)
+	.orient("left");
+	
+
+	
+var svg = d3.select("body").append("svg")
+    .attr("width", w + margin.left + margin.right)
+    .attr("height", h + margin.top + margin.bottom);
+svg.append("g")
+	.attr("class", "axis")
+    .attr("transform", "translate(" + margin.left+ "," + (h + margin.top) + ")")
+	.call(xAxis);
+  
+svg.append("g")
+	.attr("class", "axis")
+	.attr("transform", "translate(" + margin.left+ "," + margin.right + ")")
+	.call(yAxis);
+	
+
+
+
+
+
+
 		});
 	});
 })();
