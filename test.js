5b3b9c49044d77a2c4c71902fa33c6d691161765
Information School <ischoolit@uw.edu>
2015-06-11 16:38:42 -0700

    simple vis for top ten players by wins

--- test.js
+++ test.js
@@ -31,25 +31,17 @@ var margin = {top: 20, right: 20, bottom: 30, left: 70};
     var h = 500 - margin.top - margin.bottom;
 	
 var format = d3.time.format("%b %Y");
-var dataset;
-/*var x = d3.scale.ordinal()
-	.domain(["Europe", "Asia", "America", "Africa", "Oceania", "Not Specified "])
-    .rangeRoundBands([0, w - margin.left - margin.right], .1);*/
-/*var x = d3.scale.linear()
-	.domain([1820, 2000])
-	.range([0, w]);*/
+var dataset = topTen;
+
+
 	
 var y = d3.scale.linear()
-        .domain([0, 68])
+        .domain([0, d3.max(dataset, function(d) { return d.Count; })])
         .range([h, 0]);
  
  var x = d3.scale.ordinal()
-    .rangeRoundBands([0, w - margin.left - margin.right], .1);
- x.domain(winCount.map(function(d) { return d.Winner; }));
-
-
-
-
+    .rangeRoundBands([0, w - margin.left - margin.right], .1)
+ 	.domain(dataset.map(function(d) { return d.Winner; }));
 
 var tooltip = d3.select("body").append("div")     
 	.attr("class", "tooltip")     
@@ -83,7 +75,48 @@ svg.append("g")
 	.attr("class", "axis")
 	.attr("transform", "translate(" + margin.left+ "," + margin.right + ")")
 	.call(yAxis);
+ 
+ var rectangles = svg.selectAll("rect")
+ .data(dataset)
+ .enter()
+ .append("rect")
+	/*.attr("cx", function(d) { return x(d["1820]) +margin.left;  })
+    .attr("cy", function(d) { return y(d["1820"]) + margin.top;  })
+    .attr("r", 4)
+    .style("stroke", "black")
+	.attr("fill", function(d) { return col(d.type);})
+	.attr("opacity", 0.5)*/ 
 	
+    .attr("x", function(d) { return x(d["Winner"]) + margin.left;  })
+    .attr("height", function(d) { return h - y(d["Count"]); })
+	.attr("y", function(d) { return y(d["Count"]) + margin.top;})
+    .attr("width", function(d){ return x.rangeBand();})
+    .style("stroke", "black")
+	.attr("fill", function(d) { return col(d["Winner"]);})
+	.attr("opacity", 0.5)
+	.on("mouseover", function(d){ 
+		//d3.select(this).attr('r', 8)
+		/*tooltip.transition()                
+			.duration(200)                
+			.style("opacity", 1)           
+		tooltip.html('Immigration Population:<b>'+d["1820"] + '</b>')
+		tooltip.style("position", "absolute")
+		tooltip.style("left", d3.event.pageX + "px")
+		console.log(d3.event.pageX)
+		console.log(d3.event.pageY);
+		tooltip.style('top', (d3.event.pageY - 28) + 'px');*/ 	
+	})   
+	.on("mouseout", function(d,i){ 
+		//d3.select(this).attr('r', 4);
+		/*tooltip.transition()
+			.duration(500)
+			.style("opacity", 0);*/
+	}); 
+
+
+
+
+
 
 
 
